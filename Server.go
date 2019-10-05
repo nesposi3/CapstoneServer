9393a751849068a5fb4e86d8e7b6cfe3979669e3
Nicholas Esposito <nesposi3@oswego.edu>
2019-10-05 11:35:56 -0400

    Added stock struct

--- Server.go
+++ Server.go
@@ -2,8 +2,17 @@ package main
 import ("fmt"
 		"github.com/gorilla/mux"
     	"net/http"
+			    //"math/rand"
   )
+
+type stock struct{
+		name string
+		price float64
+		numShares int
+	}
+
 func main(){
+	fmt.Print(jame.name)
   r := mux.NewRouter()
   r.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Welcome to my website!")

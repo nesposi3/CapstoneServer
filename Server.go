f73127583326b58dab82fe3ce1391031a303b790
Nicholas Esposito <nesposi3@oswego.edu>
2019-10-12 13:35:44 -0400

    Added some helper functions for various structs

--- Server.go
+++ Server.go
@@ -9,13 +9,22 @@ import (
 	//"math/rand"
 )
 
+type dividend struct {
+	boughtStock stock
+	numShares   int
+}
+
 type player struct {
-	name string
+	name         string
+	deleted      bool
+	passwordHash int
+	portfolio    []dividend
 }
 type gamestate struct {
-	gameID  string
-	players []player
-	stocks  []stock
+	gameID     string
+	players    []player
+	stocks     []stock
+	tickLength int
 }
 
 //Prices in integer cents to avoid floating point comparisons etc.
@@ -23,6 +32,32 @@ type stock struct {
 	name      string
 	price     int
 	numShares int
+}
+
+// Returns an int representing the total value of a player's portfoilio
+func (p *player) getAccountBalance() int {
+	var bal = 0
+	for _, d := range p.portfolio {
+		bal = bal + (d.numShares * (d.boughtStock.price))
+	}
+	return bal
+}
+func (p *player) ownsStock(s stock) (bool, int) {
+	for i, d := range p.portfolio {
+		if d.boughtStock.equals(s) {
+			return true, i
+		}
+	}
+	return false, -1
+}
+
+//Returns info about a certain stock and a player. If the player owns that stock, returns how many shares and its price
+func (p *player) getStockInfo(s stock) (int, int) {
+	owns, index := p.ownsStock(s)
+	if owns {
+		return p.portfolio[index].numShares, p.portfolio[index].boughtStock.price
+	}
+	return -1, -1
 }
 
 // Directly changes the price of a stock
@@ -44,6 +79,37 @@ func (s *stock) waitAndChange() {
 		time.Sleep(2 * time.Second)
 		s.price += 3
 	}
+}
+func (s *stock) equals(other stock) bool {
+	return (s.name == other.name)
+}
+
+//Adds a player to the game
+func (game *gamestate) addPlayer(newPlayer player) {
+	game.players = append(game.players, newPlayer)
+}
+
+//Sets a player to deleted state
+func (game *gamestate) removePlayer(oldPlayer player) {
+	for _, p := range game.players {
+		if p.name == oldPlayer.name {
+			p.deleted = true
+		}
+	}
+}
+
+//Returns a tuple of active and deleted players
+func (game *gamestate) getNum() (int, int) {
+	var i = 0
+	var j = 0
+	for _, p := range game.players {
+		if !p.deleted {
+			i = i + 1
+		} else {
+			j = j + 1
+		}
+	}
+	return i, j
 }
 func main() {
 	r := mux.NewRouter()

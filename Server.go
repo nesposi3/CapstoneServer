0d2b4c4e745941300e1f5e1a010d2512b58a5564
Nicholas Esposito <nesposi3@oswego.edu>
2019-10-20 15:05:57 -0400

    Added a statistical function to change gamestate

--- Server.go
+++ Server.go
@@ -2,15 +2,17 @@ package main
 
 import (
 	"fmt"
+	"math"
+	"math/rand"
 	"net/http"
 	"time"
 
 	"github.com/gorilla/mux"
-	//"math/rand"
 )
 
 type dividend struct {
 	boughtStock stock
+	name        string
 	numShares   int
 }
 
@@ -29,9 +31,11 @@ type gamestate struct {
 
 //Prices in integer cents to avoid floating point comparisons etc.
 type stock struct {
-	name      string
-	price     int
-	numShares int
+	name              string
+	price             int
+	numShares         int
+	previousNumShares int
+	trend             bool
 }
 
 // Returns an int representing the total value of a player's portfoilio
@@ -65,6 +69,12 @@ func (s *stock) changePrice(price int) {
 	s.price = price
 }
 
+// Change the price by change/1000
+func (s *stock) changePriceByPermill(change int) {
+	newPrice := int(math.Round(float64(s.price) * (float64(change) / 1000.0)))
+	s.changePrice(newPrice)
+}
+
 //Directly changes the name of a stock
 func (s *stock) changeName(name string) {
 	s.name = name
@@ -74,10 +84,30 @@ func (s *stock) changeName(name string) {
 func (s *stock) changeShares(shares int) {
 	s.numShares = shares
 }
-func (s *stock) waitAndChange() {
-	for {
-		time.Sleep(2 * time.Second)
-		s.price += 3
+
+// TODO Adjust numbers
+func (s *stock) statisticalUpdate() {
+
+	// First phase of stock adjustment, if huge change in shares bought, price changes. Change ratio in perMille
+	changeRatio := int(((float64(s.numShares) - float64(s.previousNumShares)) / float64(s.numShares)) * 1000)
+	s.changePriceByPermill(changeRatio)
+	// Second phase, semi-random adjustment
+	num := rand.Intn(1000)
+	sign := 1
+	if !s.trend {
+		sign = -1
+	}
+	// 90% chance we do nothing drastic, 1% change
+	if num < 901 {
+		changePerMill := sign * 10
+		s.changePriceByPermill(changePerMill)
+	} else if num > 900 && num < 951 {
+		// 5% chance we  have a big change, 10% change
+		changePerMill := sign * 100
+		s.changePriceByPermill(changePerMill)
+	} else if num > 950 && num < 1001 {
+		// Change the trend of the stock
+		s.trend = !s.trend
 	}
 }
 func (s *stock) equals(other stock) bool {
@@ -87,6 +117,17 @@ func (s *stock) equals(other stock) bool {
 //Adds a player to the game
 func (game *gamestate) addPlayer(newPlayer player) {
 	game.players = append(game.players, newPlayer)
+}
+func (game *gamestate) updateStocks() {
+	for _, s := range game.stocks {
+		s.statisticalUpdate()
+	}
+}
+func waitAndUpdate(gameList []gamestate) {
+	time.Sleep(time.Minute)
+	for _, game := range gameList {
+		game.updateStocks()
+	}
 }
 
 //Sets a player to deleted state
@@ -115,6 +156,10 @@ func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Welcome")
+	})
+	r.HandleFunc("/register/{name}-{passHash}", func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		fmt.Printf(vars["name"])
 	})
 	http.Handle("/", r)
 	// Go keyword launches the function in another thread
